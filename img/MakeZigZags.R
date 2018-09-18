cdd262d362db50e2dc1b6abe687d65742df26e87
Tom Booker <t.r.booker@sms.ed.ac.uk>
2018-09-18 09:54:57 -0700

    changed images

--- img/MakeZigZags.R
+++ img/MakeZigZags.R
@@ -16,6 +16,8 @@ png("~/Work/TBooker.github.io/img/bigzigzags.png", width = 65, height = 65)
 
 ggplot(data = zigzags, aes ( x = x, y = y, group = z))+
   geom_ribbon(aes(ymax = y+0.5, ymin = y-0.5))+
+  scale_x_continuous( expand = c(0,0) )+
+  scale_y_continuous( expand = c(0,0) )+
   theme_bw() +
   theme(panel.border = element_blank(),
         panel.grid.major = element_blank(),
@@ -32,8 +34,8 @@ png("~/Work/TBooker.github.io/img/lilzigzags.png", width = 65, height = 65)
 ggplot(data = zigzags, aes ( x = x, y = y, group = z))+
   geom_ribbon(aes(ymax = y+0.5, ymin = y-0.5))+
   theme_bw() +
-  scale_x_continuous(limits = c(0,13))+
-  scale_y_continuous(limits = c(0,13))+
+  scale_x_continuous(limits = c(0,13), expand = c(0,0) )+
+  scale_y_continuous(limits = c(0,13), expand = c(0,0) )+
   theme(panel.border = element_blank(),
         panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
