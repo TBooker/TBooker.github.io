93d978704675cb367d262ba2098cda3177598af6
Tom Booker <t.r.booker@sms.ed.ac.uk>
2018-09-17 17:58:50 -0700

    added bg figure

---
+++ img/MakeZigZags.R
@@ -0,0 +1,24 @@
+library(ggplot2)
+
+zigzag_list = list()
+
+for (line in seq(40)){
+  y <- c(rep(c(0,1),25),0) + (line*2)
+  x <- rep(seq(51), 1)
+  z <- as.factor(line)
+  zz <- data.frame(y,x,z)
+  zigzag_list[[line]] <- zz # add it to your list
+  
+  }
+zigzags = do.call(rbind, zigzag_list)
+
+ggplot(data = zigzags, aes ( x = x, y = y, group = z))+
+  geom_ribbon(aes(ymax = y+0.5, ymin = y-0.5))+
+  theme_bw() +
+  theme(panel.border = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        axis.line =  element_blank(),
+        axis.title =  element_blank(),
+        axis.text =  element_blank(),
+        axis.ticks =  element_blank())
