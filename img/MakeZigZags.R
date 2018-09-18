02600b8816e402e76f01f5ca35104535d7b4c5bb
Tom Booker <t.r.booker@sms.ed.ac.uk>
2018-09-18 09:41:40 -0700

    addedZigs

--- img/MakeZigZags.R
+++ img/MakeZigZags.R
@@ -12,6 +12,8 @@ for (line in seq(40)){
   }
 zigzags = do.call(rbind, zigzag_list)
 
+png("~/Work/TBooker.github.io/img/bigzigzags.png", width = 65, height = 65)
+
 ggplot(data = zigzags, aes ( x = x, y = y, group = z))+
   geom_ribbon(aes(ymax = y+0.5, ymin = y-0.5))+
   theme_bw() +
@@ -22,3 +24,21 @@ ggplot(data = zigzags, aes ( x = x, y = y, group = z))+
         axis.title =  element_blank(),
         axis.text =  element_blank(),
         axis.ticks =  element_blank())
+dev.off()
+
+
+png("~/Work/TBooker.github.io/img/lilzigzags.png", width = 65, height = 65)
+
+ggplot(data = zigzags, aes ( x = x, y = y, group = z))+
+  geom_ribbon(aes(ymax = y+0.5, ymin = y-0.5))+
+  theme_bw() +
+  scale_x_continuous(limits = c(0,13))+
+  scale_y_continuous(limits = c(0,13))+
+  theme(panel.border = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        axis.line =  element_blank(),
+        axis.title =  element_blank(),
+        axis.text =  element_blank(),
+        axis.ticks =  element_blank())
+dev.off()
